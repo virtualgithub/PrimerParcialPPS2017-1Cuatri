8b5d9a4b1c1dd93682562cd834db6dc48404aec9
virtualgithub <elias.rod@live.com>
2017-04-29 01:51:06 -0300

    CORREGIDO FORM DE HOME CON FORMBUILDER PARA AGREGAR VALIDACION DEL NOMBRE

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';//FORMBUILDER CREA FORMS, FORMGROUP DEFINE UN FORMULARIO Y VALIDATORS CONTIENE VALIDACIONES PREDISEÑADAS
 import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';//STORAGE FOR IONIC
 
@@ -13,9 +15,16 @@ import { Jugada } from "../../classes/jugada";
 export class HomePage {
 
   jugadas : Jugada[];
+  miForm : FormGroup;
+  errorEnFormulario: boolean;
 
-  constructor(public navCtrl: NavController, private storage: Storage) {
+  constructor(public navCtrl: NavController, private storage: Storage, public formBuilder: FormBuilder) {
     this.jugadas = new Array();
+    this.errorEnFormulario = false;
+    //UTILIZACIÓN DE CONSTRUCTOR DE FORMULARIOS CON VALIDACIONES
+    this.miForm = formBuilder.group({
+        nombre: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
     //PREPARACIÓN DEL ALMACENAMIENTO
     this.storage.ready().then(() => {
       this.storage.get('jugadas').then((val) => {
@@ -27,17 +36,24 @@ export class HomePage {
     });
   }
 
-  irGame(nombre){
-    //CREACIÓN DE JUGADA Y AGREGADO DE JUGADA EN ARRAY DE JUGADAS
-    new Jugada(nombre).AgregarJugada(this.jugadas);
-    //PREPARACIÓN DEL ALMACENAMIENTO
-    this.storage.ready().then(() => {
-      //GUARDADO DE LAS JUGADAS EN BASE DE DATOS
-      this.storage.set('jugadas', JSON.stringify(this.jugadas)).then(() => {
-        //REDIRECCION A PAGINA DE GAME (SETEO COMO PAGINA INICIAL)
-        this.navCtrl.setRoot(Game, {}, {animate: true, direction: "forward"});
-      });      
-    });
+  irGame(){
+    //SI EL FORM ES VALIDO
+    if (this.miForm.valid) {
+      //CREACIÓN DE JUGADA Y AGREGADO DE JUGADA EN ARRAY DE JUGADAS
+      new Jugada(this.miForm.value.nombre).AgregarJugada(this.jugadas);
+      //PREPARACIÓN DEL ALMACENAMIENTO
+      this.storage.ready().then(() => {
+        //GUARDADO DE LAS JUGADAS EN BASE DE DATOS
+        this.storage.set('jugadas', JSON.stringify(this.jugadas)).then(() => {
+          //REDIRECCION A PAGINA DE GAME (SETEO COMO PAGINA INICIAL)
+          this.navCtrl.setRoot(Game, {}, {animate: true, direction: "forward"});
+        });      
+      });
+    }
+    //SETEO DE VARIABLE ERROR SI EL FORM NO ES VALIDO
+    else{
+      this.errorEnFormulario = true;
+    }
   }
 
   irAbout(){
